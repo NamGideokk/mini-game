5cd6a56801c5a323f95f45c0c96c4d1ad6c4be27
Nam Gideok <namgd1222@gmail.com>
2022-06-15 23:54:18 +0900

    미니 게임 커밋

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function loadItems() {
+  return fetch("data/data.json")
+    .then((response) => response.json())
+    .then((json) => json.items);
+}
+
+loadItems()
+  .then((items) => {
+    console.log(items);
+  })
+  .catch(console.log);
